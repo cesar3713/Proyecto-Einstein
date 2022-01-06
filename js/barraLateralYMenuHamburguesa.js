b26103547fa3edc582a6bb69bfe35972e6476d96
Cesar <cesar3713@outlook.com>
2022-01-06 10:51:30 -0300

    avanzando

---
+++ js/barraLateralYMenuHamburguesa.js
@@ -0,0 +1,30 @@
+let barraLateral = document.querySelector(".columna-navegacion")
+let navigation = document.querySelector(".pag-Navegacion")
+let menutoggle = document.querySelector(".toggle");
+
+    /*Hover en barra lateral y en menu hamburguesa*/
+
+const mouseDentro = () => {
+menutoggle.classList.add("hover-menu");
+}
+const mouseFuera = () => {
+menutoggle.classList.remove("hover-menu");
+}
+
+barraLateral.addEventListener("mouseenter",mouseDentro);
+barraLateral.addEventListener("mouseleave",mouseFuera);
+menutoggle.addEventListener("mouseenter",mouseDentro);
+menutoggle.addEventListener("mouseleave",mouseFuera);
+
+    /*Funcion Menu hamburguesa*/
+
+menutoggle.onclick = function(){
+    menutoggle.classList.toggle("active");
+    navigation.classList.toggle("active");
+}
+
+barraLateral.onclick = function(){
+    menutoggle.classList.toggle("active");
+    navigation.classList.toggle("active");
+}
+
